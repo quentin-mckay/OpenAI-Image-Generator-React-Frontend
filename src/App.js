b3d71e70e9153db77f02ed41bcefaa536b0dc56b
Quentin McKay <quentinmckay1@gmail.com>
2023-01-17 14:03:13 +1100

    added error message and minor style changes

--- src/App.js
+++ src/App.js
@@ -38,14 +38,20 @@ export default function App() {
 				},
 				body: JSON.stringify(formData)
 			})
-			.then(res => res.json())
+			.then(response => {
+				if (!response.ok) {
+					setShowSpinner(false)
+					throw new Error("Image could not be generated")
+				}
+				return response.json()
+			})
 			.then(data => {
 				setData(data)
 				setShowSpinner(false)
 			})
 
 		} catch (error) {
-			
+			setErrorMessage(error)
 		}
 	}
 	
@@ -57,8 +63,8 @@ export default function App() {
 					
 					<textarea
 						name="prompt"
-						placeholder='Prompt...'
-						className='rounded w-full text-gray-500 border-gray-400 font-bold placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-sky-500'
+						placeholder='Enter prompt...'
+						className='rounded w-full text-gray-600 border-gray-400 font-bold placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-sky-500 required'
 						onChange={handleChange}
 						value={formData.prompt}
 						></textarea>
@@ -67,7 +73,7 @@ export default function App() {
 
 						<select
 							name="size"
-							className='rounded border-gray-400 text-gray-500 focus:outline-none focus:ring-1 focus:ring-sky-500 '
+							className='rounded border-gray-400 text-gray-600 focus:outline-none focus:ring-1 focus:ring-sky-500 '
 							value={formData.size}
 							onChange={handleChange}
 							>
@@ -83,12 +89,17 @@ export default function App() {
 							className='w-full border border-gray-500 rounded p-2 px-4 font-bold text-white
 							border-none bg-green-600 hover:cursor-pointer hover:bg-green-500 active:bg-green-400 transition'/>
 					</div>
+					
 				</form>
-			
+
+
 				{data.image_url && <img src={data.image_url} alt="generated" className='w-full p-6 pt-0' />}
 			</main>
+			
 
 			{showSpinner && <Spinner />}
+
+			{errorMessage && <p className='bg-red-100 rounded-lg py-5 px-6 mb-4 text-base text-red-700 mb-3'>{errorMessage}</p>}
 			
 		</div>
 	)
